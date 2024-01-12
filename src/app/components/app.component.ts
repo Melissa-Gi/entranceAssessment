28cc8275d667464fda912fac6d76fe5cc1e2f05c
MelissaG <gthmel001@myuct.ac.za>
2024-01-12 14:34:01 +0200

    The app component is displaying

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -3,15 +3,11 @@ import { StudentListComponent } from './student-list.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 @Component({
+  standalone:true,
   selector: 'app-root',
   templateUrl:'./app.component.html',
+  imports: [RouterLink, RouterOutlet, RouterLinkActive,RouterModule],
 })
-export class AppComponent implements OnInit {
- 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void { }
-  get getCurrentURL() {
-    return this.router.url; }
-}
+export class AppComponent {}
